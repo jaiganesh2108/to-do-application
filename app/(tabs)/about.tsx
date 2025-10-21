eb3891210cf53420eabfa88eefd7680c5297ddbe
jaiganesh2108 <jaig7335@gmail.com>
2025-10-21 08:01:40 +0530

    update commit

--- app/(tabs)/about.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text} from 'react-native';
-const About = () => {
-    return(
-        <View>
-            <Text>About Screen</Text>
-        </View>
-    )
-}
-export default About;
---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const About = () => {
+  return (
+    <View>
+      <Text>This is the about screen!</Text>
+    </View>
+  )
+}
+
+export default About
