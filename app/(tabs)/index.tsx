0d949f3dcfda59766061e8a5f1e5e4e81231e7ed
jaiganesh2108 <jaig7335@gmail.com>
2025-10-22 21:28:24 +0530

    backend integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
 import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
 import  useTheme  from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+  const todos = useQuery(api.todos.getTodos)
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo)
+
   return(
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen!</Text>
-      <TouchableOpacity onPress={ toggleDarkMode }><Text>toggle the mode!</Text></TouchableOpacity>
+      <TouchableOpacity onPress={ () => addTodo({text:"walk the dog"}) }><Text>Add a new TODO!</Text></TouchableOpacity>
     </View>
   );
 }
