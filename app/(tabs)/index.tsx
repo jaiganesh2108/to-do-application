b47aaec4b5e52a536e3b822958f9280c85c35d1b
jaiganesh2108 <jaig7335@gmail.com>
2025-10-25 12:33:10 +0530

    last commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,15 +6,10 @@ import { api } from "@/convex/_generated/api";
 export default function Index() {
   const {toggleDarkMode} = useTheme();
 
-  const todos = useQuery(api.todos.getTodos)
-  console.log(todos);
-
-  const addTodo = useMutation(api.todos.addTodo)
-
   return(
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen!</Text>
-      <TouchableOpacity onPress={ () => addTodo({text:"walk the dog"}) }><Text>Add a new TODO!</Text></TouchableOpacity>
+      <TouchableOpacity onPress={ toggleDarkMode }><Text>Toggle the mode!</Text></TouchableOpacity>
     </View>
   );
 }
