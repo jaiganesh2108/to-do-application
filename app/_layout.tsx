3892664f22422885ba9e6bf499137299a8da6662
jaiganesh2108 <jaig7335@gmail.com>
2025-12-06 15:30:51 +0530

    full project completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/hooks/useTheme";
-import { Stack } from "expo-router";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Stack } from "expo-router";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
@@ -9,13 +9,11 @@ const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
 export default function RootLayout() {
   return (
     <ConvexProvider client={convex}>
-        <ThemeProvider>
-          <Stack screenOptions={{headerShown:false}}>
-            <Stack.Screen name="(tabs)" />
-          </Stack>
-        </ThemeProvider>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
     </ConvexProvider>
-  
   );
 }
-  
--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -1,6 +1,7 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
+// AsyncStorage is React Native’s simple, promise-based API for persisting small bits of data on a user’s device. Think of it as the mobile-app equivalent of the browser’s localStorage, but asynchronous and cross-platform.
 
 export interface ColorScheme {
   bg: string;
@@ -86,42 +87,46 @@ const darkColors: ColorScheme = {
   statusBarStyle: "light-content" as const,
 };
 
-interface ThemeContextType{
-    isDarkMode: boolean;
-    toggleDarkMode: () => void; 
-    colors: ColorScheme
+interface ThemeContextType {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+  colors: ColorScheme;
 }
 
-const ThemeContext = createContext<undefined | ThemeContextType>(undefined)
+const ThemeContext = createContext<undefined | ThemeContextType>(undefined);
 
-export const ThemeProvider = ({children}: {children: React.ReactNode}) => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    
-    useEffect(() => {
-        //get the users choice from async storage
-        AsyncStorage.getItem('darkMode').then((value) => {
-            if (value) setIsDarkMode(JSON.parse(value));
-        });
-    }, []);
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-    const toggleDarkMode = async() => {
-        const newMode = !isDarkMode;
-        setIsDarkMode(newMode);
-        await AsyncStorage.setItem("darkMode", JSON.stringify(newMode));
-    };
-    const colors = isDarkMode ? darkColors : lightColors;
-    return(
-        <ThemeContext.Provider value={{isDarkMode, toggleDarkMode, colors}}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  useEffect(() => {
+    // get the user's choice
+    AsyncStorage.getItem("darkMode").then((value) => {
+      if (value) setIsDarkMode(JSON.parse(value));
+    });
+  }, []);
+
+  const toggleDarkMode = async () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    await AsyncStorage.setItem("darkMode", JSON.stringify(newMode));
+  };
+
+  const colors = isDarkMode ? darkColors : lightColors;
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode, colors }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
+
 const useTheme = () => {
-    const context = useContext(ThemeContext)
-    if(context === undefined){
-        throw new Error("useTheme must be used within a ThemeProvider");
-    }
-    return context;
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return context;
 };
 
-export default useTheme
+export default useTheme;
